3575ab5d0093d21d4ed4ce02b208ad4b9cd33907
Pavel Valger <pavelwalker@mail.ru>
2023-09-16 19:56:46 +0500

    6. Лямбды и шаблон Command [#49295 #445250]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -1,65 +1,15 @@
 package ru.job4j.cars.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import ru.job4j.cars.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 @AllArgsConstructor
 public class UserRepository {
-    private final SessionFactory sf;
-
-    /**
-     * Зафиксировать изменения и закрыть сессию.
-     *
-     * @param session сессия.
-     */
-    private void close(Session session) {
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    /**
-     * Сессия для операций без возвращаемого значения.
-     *
-     * @param consumer применяет действие.
-     */
-    private void crud(Consumer<Session> consumer) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            consumer.accept(session);
-            close(session);
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-    }
-
-    /**
-     * Сессия для операций с возвращаемым значением.
-     *
-     * @param function преобразование входных параметров.
-     * @param <T>      тип вычисления функции.
-     * @return результат вычисления функции.
-     */
-    private <T> T search(Function<Session, T> function) {
-        Session session = sf.openSession();
-        T result = null;
-        try {
-            session.beginTransaction();
-            result = function.apply(session);
-            close(session);
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return result;
-    }
+    private final CrudRepository crudRepository;
 
     /**
      * Сохранить в базе.
@@ -68,7 +18,7 @@ public class UserRepository {
      * @return пользователь с id.
      */
     public User create(User user) {
-        crud(session -> session.save(user));
+        crudRepository.run(session -> session.persist(user));
         return user;
     }
 
@@ -78,7 +28,7 @@ public class UserRepository {
      * @param user пользователь.
      */
     public void update(User user) {
-        crud(session -> session.update(user));
+        crudRepository.run(session -> session.merge(user));
     }
 
     /**
@@ -87,10 +37,10 @@ public class UserRepository {
      * @param userId ID
      */
     public void delete(int userId) {
-        crud(session -> session
-                .createQuery("DELETE User WHERE id = :fId")
-                .setParameter("fId", userId)
-                .executeUpdate());
+        crudRepository.run(
+                "delete User where id = :fId",
+                Map.of("fId", userId)
+        );
     }
 
     /**
@@ -99,7 +49,7 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findAllOrderById() {
-        return new ArrayList<>(search(session -> session.createQuery("from User", User.class).list()));
+        return crudRepository.query("from User order by id asc", User.class);
     }
 
     /**
@@ -108,7 +58,10 @@ public class UserRepository {
      * @return пользователь.
      */
     public Optional<User> findById(int userId) {
-        return search(session -> Optional.ofNullable(session.get(User.class, userId)));
+        return crudRepository.optional(
+                "from User where id = :fId", User.class,
+                Map.of("fId", userId)
+        );
     }
 
     /**
@@ -118,10 +71,10 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findByLikeLogin(String key) {
-        return new ArrayList<>(search(session -> session
-                .createQuery("from User where login like :fKey", User.class)
-                .setParameter("fKey", String.format("%%%s%%", key))
-                .list()));
+        return crudRepository.query(
+                "from User where login like :fKey", User.class,
+                Map.of("fKey", String.format("%%%s%%", key))
+        );
     }
 
     /**
@@ -131,9 +84,9 @@ public class UserRepository {
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
-        return search(session -> session
-                .createQuery("from User as i where i.login = :fLogin", User.class)
-                .setParameter("fLogin", login)
-                .uniqueResultOptional());
+        return crudRepository.optional(
+                "from User where login = :fLogin", User.class,
+                Map.of("fLogin", login)
+        );
     }
 }
