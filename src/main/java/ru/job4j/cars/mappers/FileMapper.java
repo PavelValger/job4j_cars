9a4a5648f497dd3094d4879083ac364c0a536870
Pavel Valger <pavelwalker@mail.ru>
2023-10-05 00:39:19 +0500

    3. Тестовое задание. Hibernate. [#330581] добавил статически ресурсы и вид индекс.

--- src/main/java/ru/job4j/cars/mappers/FileMapper.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.cars.mappers;
-
-import org.mapstruct.InheritInverseConfiguration;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-@Mapper(componentModel = "spring")
-public interface FileMapper {
-}
